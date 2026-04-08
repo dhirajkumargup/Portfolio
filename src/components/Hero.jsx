d02c133cc1054ebcd768e8022983b0f881df5dd7
Dhirajkumar111 <dhiraj.kumar@Cyberfrat.com>
2026-04-08 11:50:19 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -86,7 +86,7 @@ export default function Hero() {
                 .scrollIntoView({ behavior: "smooth" })
               }
             >
-            ram s
+            Connect me
             </button>
 
             <button
