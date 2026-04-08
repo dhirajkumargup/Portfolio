f225ab87494148781a104800803ba404c2216494
Dhirajkumar111 <dhiraj.kumar@Cyberfrat.com>
2026-04-08 10:46:26 +0530

    fix router path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/Portfolio">
-    <App />
-  </BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter basename="/Portfolio">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
